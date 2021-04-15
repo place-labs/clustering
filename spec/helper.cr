df060094cb067e4fd9f7f110c2c825d7bb6b685e
Caspian Baska <caspianbaska@gmail.com>
2021-04-15 12:26:54 +1000

    chore: refresh spec

--- spec/helper.cr
+++ spec/helper.cr
@@ -2,10 +2,22 @@ require "spec"
 require "../src/example/node"
 require "../src/clustering"
 
-backend = Log::IOBackend.new
-backend.formatter = Log::Formatter.new do |entry, io|
-  io << entry.severity.to_s[0] << ": " << entry.message << " - "
-  io << entry.context.to_s
+Spec.before_each do
+  HoundDog.etcd_client do |client|
+    client.kv.delete_prefix("cluster")
+    client.kv.delete_prefix("service")
+  end
+  sleep 1
 end
 
-Log.builder.bind "*", :debug, backend
+Spec.before_suite do
+  backend = Log::IOBackend.new
+  backend.formatter = Log::Formatter.new do |entry, io|
+    io << entry.severity.to_s[0] << ": "
+    io << entry.message << " - " unless entry.message.presence.nil?
+    io << entry.context.join(", ") { |k, v| "#{k}=#{v.inspect}" }
+    io << " " << entry.data.join(", ") { |k, v| "#{k}=#{v.inspect}" }
+  end
+
+  Log.setup "*", :trace, backend
+end
